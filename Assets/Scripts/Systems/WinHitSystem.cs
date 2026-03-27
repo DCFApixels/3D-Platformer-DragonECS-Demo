e7bd09d1705e645eddc83d95d9b43a40aad8d610
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2026-03-28 02:17:45 +0800

    Update WinHitSystem.cs

--- Assets/Scripts/Systems/WinHitSystem.cs
+++ Assets/Scripts/Systems/WinHitSystem.cs
@@ -30,7 +30,10 @@ namespace Platformer
                     if (hit.Other.CompareTag(Constants.Tags.WinPointTag))
                     {
                         player.Transform.gameObject.SetActive(false);
-                        _world.DelEntity(playerE);
+                        if (_world.IsUsed(playerE))
+                        {
+                            _world.DelEntity(playerE);
+                        }
                         _gameData.S.UI.WinScreen.gameObject.SetActive(true);
                     }
                 }
